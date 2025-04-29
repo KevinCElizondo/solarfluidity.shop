e71d471694fd06734effc782b95f8e6c187134c1
Kevin Cordero <kevincordero@solarfluidity.com>
2025-04-28 22:27:55 -0600

    Update affiliate links

---
+++ app/src/config/affiliateLinks.ts
@@ -0,0 +1,54 @@
+export const AMAZON_TAG = "1405250224061-20";\n\nexport const affiliateLinks = {\n  panelesSolares: [\n    {\n      name: "Panel Solar Monocristalino 400W",\n      link: "https://amzn.to/4jYnK4a",\n    },\n    {\n      name: "Panel Solar Monocristalino 400W (2)",\n      link: "https://amzn.to/4jABzG0",\n    },\n    {\n      name: "Panel Solar Monocristalino 500W",\n      link: "https://amzn.to/4jOJX53",\n    },\n  ],
+  baterias: [
+    {
+      name: "Batería de Litio 48V 200Ah (18)",
+      link: "https://amzn.to/44OKCP1",
+    },
+    {
+      name: "Batería AGM 12V 150Ah Ciclo Profundo",
+      link: "https://amzn.to/44gO9pe",
+    },
+  ],
+  inversores: [
+    {
+      name: "Inversor Híbrido 3kW 48V (22)",
+      link: "https://amzn.to/3YkerTC",
+    },
+  ],
+  kitsSolares: [
+    {
+      name: "Kit Solar 5kW Residencial Completo (16)",
+      link: "https://amzn.to/4lSCoLN",
+    },
+  ],
+  accesorios: [
+    {
+      name: "Accesorios solares",
+      link: "https://amzn.to/433BULs",
+    },
+  ],
+  monitoreo: [
+    {
+      name: "Monitor de Energía WiFi",
+      link: "https://amzn.to/4jNhVGs",
+    },
+  ],
+  relojesGarmin: [
+    {
+      name: "Garmin Instinct 2 Solar, GPS Outdoor Watch (1247",
+      link: "https://amzn.to/44IeBIj",
+    },
+    {
+      name: "Garmin",
+      link: "https://amzn.to/42yR1MR",
+    },
+    {
+      name: "Garmin Fénix 7 Solar, Reloj Multideporte Premium",
+      link: "https://amzn.to/42VRfg0",
+    },
+    {
+      name: "Garmin Enduro 2, Reloj Ultra Resistencia con Solar",
+      link: "https://www.amazon.com/Garmin-EnduroTM-Ultraperformance-Long-Lasting-Preloaded/dp/B09ZZ7Y8WZ/ref=sr_1_1?crid=78HXQ0D9OMDL&dib=eyJ2IjoiMSJ9.BxWRUG3oTLT03hXXvQIKzfLCW1iCqRfnMYBXDNJdxCqg7aVsvSmwffUuEGdOGFoQWipGaCuOkt36Poxtd3yZ2V8yUyKoguktb2m5-iUd2mU73I8WiDVHtkY6k4qSMewO8qMYvemaqw5zhT-6zlFDXRKXCsGzworHC31DPEtLa28fqPK-eFIMo7lsM7gUvXiZYB8XMysvU1LOb9nltnkUNar2t09aqwO0bZlQJ2MihAA.fo9zqzItwd4p_ooMWcO0a2CZoHI3h05LEjCAIBdRNJA&dib_tag=se&keywords=Garmin+Enduro+2%2C+Reloj+Ultra+Resistencia+con+solar&qid=1745899234&sprefix=garmin+enduro+2%2C+reloj+ultra+resistencia+con+solar%2Caps%2C355&sr=8-1",
+    }
+  ]
+};
